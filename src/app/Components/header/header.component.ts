99aee62b87a3aecb4a55f0e3fb4b180a04a89edf
Josué Aquino <aquinojosue889@gmail.com>
2025-05-28 20:43:14 -0300

    Se creó el dropdown para el header en diseño responsivo para mobile

--- src/app/Components/header/header.component.ts
+++ src/app/Components/header/header.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { DropdownComponent } from './dropdown/dropdown.component';
+import { NgClass, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [DropdownComponent, NgIf],
+  standalone: true,
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+  public dropdown: boolean = true;
 
+  togleMenu() {
+    this.dropdown = !this.dropdown;
+  }
 }
