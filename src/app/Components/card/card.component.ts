a667631f39302dc3cc6ff714582848d2cc80af8f
Josué Aquino <aquinojosue889@gmail.com>
2025-05-29 18:28:00 -0300

    Se hizo el refactor del component Card para utilizar como card de productos en diseño responsivo para mobile

--- src/app/Components/card/card.component.ts
+++ src/app/Components/card/card.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { TitleComponent } from '../../MicroComponents/title/title.component';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-card',
-  imports: [TitleComponent],
+  imports: [TitleComponent, NgIf],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
 export class CardComponent {
-
+  @Input() image:string = '';
+  @Input() title:string = '';
+  @Input() is_product!:boolean;
 }
