30c6e0ae0791e615371faf5186bdeac5ef54a2d6
Josué Aquino <aquinojosue889@gmail.com>
2025-05-29 14:56:12 -0300

    Se creó el form para los turnos en diseño responsivo para mobile

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { MysliderComponent } from '../../MicroComponents/myslider/myslider.component';
 import { CardComponent } from '../../Components/card/card.component';
+import { TitleComponent } from '../../MicroComponents/title/title.component';
+import { FormComponent } from '../../Components/form/form.component';
 
 @Component({
   selector: 'app-home',
-  imports: [MysliderComponent, CardComponent],
+  imports: [MysliderComponent, CardComponent, TitleComponent, FormComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
