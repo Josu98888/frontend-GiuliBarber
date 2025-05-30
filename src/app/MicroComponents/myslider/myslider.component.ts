111a1b8c703eac652fc15eea488ebe8fb8e56acb
Josué Aquino <aquinojosue889@gmail.com>
2025-05-30 05:25:05 -0300

    Se creó la funcionalidad de myslider

--- src/app/MicroComponents/myslider/myslider.component.ts
+++ src/app/MicroComponents/myslider/myslider.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ContentChildren,
+  ElementRef,
+  QueryList,
+  ViewChild,
+  AfterViewInit,
+} from '@angular/core';
 
 @Component({
   selector: 'app-myslider',
   imports: [],
   templateUrl: './myslider.component.html',
-  styleUrl: './myslider.component.css'
+  styleUrl: './myslider.component.css',
 })
-export class MysliderComponent {
+export class MysliderComponent implements AfterViewInit {
+  @ViewChild('slider', { static: true }) slider!: ElementRef;
+  @ContentChildren('slideItem', { descendants: true })
+  slideItems!: QueryList<ElementRef>;
+  currentIndex: number = 0;
 
+  ngAfterViewInit(): void {
+    this.updateSlider();
+  }
+  prev() {
+    this.currentIndex =
+      (this.currentIndex - 1 + this.slideItems.length) % this.slideItems.length;
+    this.updateSlider();
+  }
+  next() {
+    this.currentIndex = (this.currentIndex + 1) % this.slideItems.length;
+    this.updateSlider();
+  }
+
+  updateSlider() {
+    if (this.slider?.nativeElement) {
+      this.slider.nativeElement.style.transform = `translateX(-${
+        this.currentIndex * 100
+      }%)`;
+    }
+  }
 }
