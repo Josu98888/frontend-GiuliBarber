870c1967cc9d5c980b43f159dc70409a3985ca65
Josué Aquino <aquinojosue889@gmail.com>
2025-05-29 21:06:13 -0300

    Se creó el Components Maps responsivo para diseño mobile

---
+++ src/app/Components/maps/maps.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-maps',
+  imports: [],
+  templateUrl: './maps.component.html',
+  styleUrl: './maps.component.css',
+})
+export class MapsComponent {}
--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -4,10 +4,11 @@ import { CardComponent } from '../../Components/card/card.component';
 import { TitleComponent } from '../../MicroComponents/title/title.component';
 import { FormComponent } from '../../Components/form/form.component';
 import { SocialNetworksComponent } from '../../Components/social-networks/social-networks.component';
+import { MapsComponent } from '../../Components/maps/maps.component';
 
 @Component({
   selector: 'app-home',
-  imports: [MysliderComponent, CardComponent, TitleComponent, FormComponent, SocialNetworksComponent],
+  imports: [MysliderComponent, CardComponent, TitleComponent, FormComponent, SocialNetworksComponent, MapsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
